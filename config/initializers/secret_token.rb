6728abf935d2a2548f10b35ab4b8c5de06325f19
Anton Trushkevich <trushkevich@gmail.com>
2013-05-18 00:34:14 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testrss::Application.config.secret_token = '8a848195fd43219a4df4fd3525e65463406f32884a55c3c392766c3cd743909c0db258758d5fd0ea79dd6a03d0fc4cdf5cc23ea55f0779557d170129fd50c160'
